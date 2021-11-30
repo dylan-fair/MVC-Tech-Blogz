73bbb8bb846984b314743fbce143f5e8148ac325
dylan fair <dylanfair11@gmail.com>
2021-11-29 19:21:08 -0600

    added the moduels and connections

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const Comment = require('./Comment');
+const Post = require('./Post');
+const User = require('./User');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+module.exports = {Comment, Post, User};
